90a227ad4644d53aaff6fa52df164e391fe960cd
Azizo1212 <azizdali530@gmail.com>
2021-10-14 08:33:50 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 0; n <= 9; n++)
-        {
-                putchar(n + '0');
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
 		putchar(44);
 		putchar(32);
         }
-        putchar('\n');
+	putchar('\n');
         return (0);
 }
