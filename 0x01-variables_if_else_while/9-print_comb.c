4b093d40b0b6668e494194b89567177b39dd10cd
Azizo1212 <azizdali530@gmail.com>
2021-10-14 08:28:04 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase, followed by a new line.
+ * Return: 0 success
+ */
+int main(void)
+{
+        int n;
+
+        for (n = 0; n <= 9; n++)
+        {
+                putchar(n + '0');
+		putchar(44);
+		putchar(32);
+        }
+        putchar('\n');
+        return (0);
+}
