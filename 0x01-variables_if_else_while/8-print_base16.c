39c901df9205759fb4f7fa7764fcef95ea68ae7f
Azizo1212 <azizdali530@gmail.com>
2021-10-14 08:14:38 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase, followed by a new line.
+ * Return: 0 success
+ */
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n + '0');
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
