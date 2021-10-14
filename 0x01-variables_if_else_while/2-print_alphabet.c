5e550b4286e30acc76971f2f3654e1f6e362a02c
Azizo1212 <azizdali530@gmail.com>
2021-10-14 06:21:25 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  * main - prints the alphabet in lowercase, followed by a new line.
  * Return: 0 success
  */
-int main (void)
+int main(void)
 {
 	char ch;
 
-	for(ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
