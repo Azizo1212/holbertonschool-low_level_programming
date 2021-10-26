80b6ae416854ce08455baf4d927a8486844cef07
Azizo1212 <azizdali530@gmail.com>
2021-10-26 04:29:50 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void _puts_recursion:prints a string, followed by a new line.
+ * @s:pointer
+ * Return: none
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+		_putchar('\n');
+}
