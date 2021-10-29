0cb0669f7d4f38f8f5c5bc4eadd55e0313462112
Azizo1212 <azizdali530@gmail.com>
2021-10-29 07:08:00 -0700

    0x04-04

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	{
 		for (digit = 0; argv[i][digit]; digit++)
 		{
-			if ( argv[i][digit] < '0' || argv[i][digit] > '9')
+			if (argv[i][digit] < '0' || argv[i][digit] > '9')
 			{
 				printf("Error\n");
 				return (1);
