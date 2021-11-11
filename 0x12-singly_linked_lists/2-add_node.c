1c8cca70bbe35b29f7c518a806407cf846bd3e5f
Azizo1212 <azizdali530@gmail.com>
2021-11-11 14:10:56 -0800

    add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,10 +14,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *add_h;
 
 
-        if (add_h == NULL)
-                return (NULL);
 	add_h = malloc(sizeof(list_t));
-
+	if (add_h == NULL)
+		return (NULL);
 	add_h->str = strdup(str);
 	add_h->len = strlen(str);
 	add_h->next = *head;
