dd2fe5fa55091f0c6be798d34fb7a1d14d98f03f
Azizo1212 <azizdali530@gmail.com>
2021-11-16 08:38:42 -0800

    print_listint

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_L
+#define MAIN_L
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
